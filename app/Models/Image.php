a0d2710c4f881fe0de9fdc176efcdb0a89264de2
rawhx_x <achmadhasbil24@gmail.com>
2025-04-04 18:37:46 +0700

    fix: user_id tidak keisi muncul

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    protected $fillable = ['judul', 'deskripsi', 'path'];
+    protected $fillable = ['judul', 'deskripsi', 'path', 'user_id'];
 }
